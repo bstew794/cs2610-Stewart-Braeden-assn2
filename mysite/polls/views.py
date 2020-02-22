1410dfcb295c5a4456adf92555fa198d4c9dd124
Braeden Stewart <braeden.stewart@aggiemail.usu.edu>
2020-02-22 15:59:52 -0700

    Created index.html for polls app

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,16 +1,17 @@
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 from django.shortcuts import render
 
 
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("you're look at question %s." % question_id)
+    return HttpResponse("you're looking at question %s." % question_id)
 
 
 def results(request, question_id):
